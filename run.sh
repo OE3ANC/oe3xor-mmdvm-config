b6e8c6a6e36448877aa15cbee5414e982738029e
OE3ANC <mail@oe3anc.at>
2024-09-01 08:23:47 +0200

    Moved image to dockerhub

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Downloading M17 Hostlist..."
+curl 'http://www.dudetronics.com/ar-dns/M17Hosts.txt' 2>/dev/null > /app/mmdvm/M17Hosts.txt
+
 echo "Starting supervisord..."
 cd /app
 supervisord -c /app/supervisor.conf
