a79c94691946a385d09809df37846bbe9ec15c27
OE3ANC <mail@oe3anc.at>
2024-10-30 16:41:10 +0100

    using mqtt branch for m17gateway

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Loading latest release of xorctl"
-wget -qO- https://github.com/OE3ANC/xorctl/releases/latest/download/xorctl-x86_64-unknown-linux-musl.tar.gz | tar xvz -C /app/
+wget -qO- https://github.com/OE3ANC/xorctl/releases/latest/download/xorctl-x86_64-unknown-linux-musl.tar.gz | tar xvz -C /app
 chmod +x /app/xorctl
 
 echo "Starting supervisord..."
