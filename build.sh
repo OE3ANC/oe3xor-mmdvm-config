69bbe54c216cc9bbe9aec4bb90197e36014f0dee
OE3ANC <mail@oe3anc.at>
2024-10-30 17:01:03 +0100

    removed unused ysf gateway

--- build.sh
+++ build.sh
@@ -4,16 +4,10 @@ cd /workdir
 
 # Clone MMDVMHost and gateway services
 git clone -b mqtt https://github.com/g4klx/MMDVMHost.git
-git clone https://github.com/g4klx/YSFClients.git
 git clone -b mqtt https://github.com/g4klx/M17Gateway.git
 
 # Build MMDVMHost
 cd MMDVMHost
-make -j8
-cd ..
-
-# Build YSFClients
-cd YSFClients
 make -j8
 cd ..
 
@@ -28,14 +22,12 @@ mkdir -p /app/mmdvm/logs
 
 # Copy binaries
 cp MMDVMHost/MMDVMHost   /app/
-cp YSFClients/YSFGateway /app/
 cp M17Gateway/M17Gateway /app/
-cp M17Gateway/M17Hosts.txt /app/mmdvm/M17Hosts.txt
+# cp M17Gateway/M17Hosts.txt /app/mmdvm/M17Hosts.txt
 cp -r M17Gateway/Audio /app/mmdvm/
 
 # Copy and rename config examples -> TODO replace log dirs
 cp MMDVMHost/MMDVM.ini       /app/mmdvm/configs/MMDVM.ini.example
-cp YSFClients/YSFGateway.ini /app/mmdvm/configs/YSFGateway.ini.example
 cp M17Gateway/M17Gateway.ini /app/mmdvm/configs/M17Gateway.ini.example
 
 ls -hal /app/mmdvm
