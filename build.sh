52ee3209aa7300a08e5d3ca6d8df7a7ee7e45d0f
Andreas Schmidberger <83550429+OE3ANC@users.noreply.github.com>
2025-02-15 15:27:38 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ cd /workdir
 # Clone MMDVMHost and gateway services
 git clone -b mqtt https://github.com/g4klx/MMDVMHost.git
 git clone -b mqtt https://github.com/g4klx/M17Gateway.git
-git clone -b mqtt https://github.com/g4klx/FMGateway.git
+git clone https://github.com/g4klx/FMGateway.git # MQTT branch has a connection problem
 
 # Build MMDVMHost
 cd MMDVMHost
@@ -14,7 +14,7 @@ cd ..
 
 # Build M17Gateway
 cd M17Gateway
-git reset --hard 94b719e2294c762442d1e427bf959b4aeb516996
+git reset --hard 94b719e2294c762442d1e427bf959b4aeb516996 # Reverting to old version, latest in mqtt branch crashes
 make -j8
 cd ..
 
